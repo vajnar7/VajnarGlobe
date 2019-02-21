ce17fdcfa827241ddcecf9915e55dad76ffcfd10
Zoran Robic <vajnar7@gmail.com>
2019-02-21 15:17:33 +0100

    Math lib, refactorisation.

--- app/src/main/java/si/vajnartech/vajnarglobe/Area.java
+++ app/src/main/java/si/vajnartech/vajnarglobe/Area.java
@@ -12,9 +12,9 @@ import si.vajnartech.vajnarglobe.math.R2Double;
 abstract class Area extends ArrayList<Line>
 {
   private String areaName;
-  Point min;
+  R2Double min;
   ArrayList<GeoPoint> geoPoints = new ArrayList<>();
-  private ArrayList<Point> points = new ArrayList<>();
+  private ArrayList<R2Double> points = new ArrayList<>();
 
   Area(String name, ArrayList<GeoPoint> p)
   {
@@ -22,7 +22,7 @@ abstract class Area extends ArrayList<Line>
     geoPoints.addAll(p);
     _sortPoints(geoPoints);
     for (GeoPoint point : geoPoints)
-      points.add(new Point(point.lon, point.lat));
+      points.add(new R2Double(point.lon, point.lat));
     min = min(points);
     min = transform(min, false);
   }
@@ -57,60 +57,63 @@ abstract class Area extends ArrayList<Line>
     return areaName;
   }
 
+  @SuppressWarnings("UnusedReturnValue")
   abstract protected Area mark(GeoPoint a);
 
-  abstract protected ArrayList<Point> process(Point p);
+  abstract protected ArrayList<R2Double> process(R2Double p);
 
   abstract public void draw(Canvas canvas, Paint paint, int color);
 
-  protected boolean isInside(Point p)
+  protected boolean isInside(R2Double p)
   {
     boolean oddNodes = false;
     int     j        = size() - 1;
     int     i;
 
     for (i = 0; i < size(); i++) {
-      double iy = get(i).p1.y;
-      double ix = get(i).p1.x;
-      double jy = get(j).p1.y;
-      double jx = get(j).p1.x;
-      if ((iy < p.y && jy >= p.y || jy < p.y && iy >= p.y) && (ix <= p.x || jx <= p.x))
-        if (ix + (p.y - iy) / (jy - iy) * (jx - ix) < p.x)
+      double iy = get(i).p1.get(1);
+      double ix = get(i).p1.get(0);
+      double jy = get(j).p1.get(1);
+      double jx = get(j).p1.get(0);
+      if ((iy < p.get(1) && jy >= p.get(1) || jy < p.get(1) && iy >= p.get(1)) && (ix <= p.get(0) || jx <= p.get(0)))
+        if (ix + (p.get(1) - iy) / (jy - iy) * (jx - ix) < p.get(0))
           oddNodes = !oddNodes;
       j = i;
     }
     return oddNodes;
   }
 
-  Point getClosestPoint(Line l, Point p)
+  R2Double getClosestPoint(Line l, R2Double p)
   {
     if (l.f.isHorizontal)
-      return new Point(p.x, l.p1.y);
+      return new R2Double(p.get(0), l.p1.get(1));
     if (l.f.isVertical)
-      return new Point(l.p1.x, p.y);
+      return new R2Double(l.p1.get(0), p.get(1));
     double a, b, c;
     a = l.f.a;
     b = l.f.b;
     c = l.f.c;
     double k  = (l.f.a * l.f.a + l.f.b * l.f.b); //a^2 + b^2
-    double qx = b * (b * p.x - a * p.y) - a * c;
-    double qy = a * (-b * p.x + a * p.y) - b * c;
-    return new Point((float) (qx / k), (float) (qy / k));
+    double qx = b * (b * p.get(0) - a * p.get(1)) - a * c;
+    double qy = a * (-b * p.get(0) + a * p.get(1)) - b * c;
+    return new R2Double(qx / k, qy / k);
   }
 
   protected abstract void save();
 
-  protected abstract Point transform(R2Double p, boolean norm);
+  @SuppressWarnings("SameParameterValue")
+  abstract R2Double transform(R2Double p, boolean norm);
 
-  private Point min(ArrayList<Point> points)
+  private R2Double min(ArrayList<R2Double> points)
   {
-    Point res = new Point(points.get(0).x, points.get(0).y);
-    for (Point o : points) {
-      if (o.x < res.x)
-        res.x = o.x;
-      if (o.y < res.y)
-        res.y = o.y;
+    double x1 = points.get(0).get(0);
+    double x2 = points.get(0).get(1);
+    for (R2Double o : points) {
+      if (o.get(0) < x1)
+        x1 = o.get(0);
+      if (o.get(1) < x2)
+        x2 = o.get(1);
     }
-    return res;
+    return new R2Double(x1, x2);
   }
 }
